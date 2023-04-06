0720dde5d2fb089114c3ba8cb46be9c91c4b49c9
lmj01 <lmj01@users.noreply.github.com>
2023-04-06 10:27:38 +0800

    update

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
 export function getBaseRoot() {
     return ['development'].includes(process.env.NODE_ENV) ? '/' : '/web/';
 } 
+
+export function getFavicon() {
+    return ['development'].includes(process.env.NODE_ENV) ? '/beta.png' : '/web/beta.png';
+} 
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import html from 'vite-plugin-html'
 import svgLoader from 'vite-svg-loader';
 import {resolve} from 'path';
-import {getBaseRoot} from './config';
+import {getBaseRoot, getFavicon} from './config';
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
@@ -18,7 +18,7 @@ export default defineConfig(({mode}) => {
         inject: {
           injectData: {
             title: '管理系统',
-            iconPath: `${configure.parsed.VITE_APP_ICON}`,
+            iconPath: `${getFavicon()}`,
           }
         },
         minify: false,

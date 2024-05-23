eb04808e57416b4466715e999ae0280e77685ca0
lmj01 <lmj01@users.noreply.github.com>
2024-05-23 11:33:50 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -10,18 +10,18 @@ const routes = [
 ]
 
 export const mapComponent2LocalFile = {
-  'IMPLANT': {
-    component: () => import('../views/ViewerImplant.vue'),
-    functionName: 'Implant',
-    functionCode: 'ViewerCT',
-    functionId: '101',
-  },
-  'CROWN': {
-    component: () => import('../views/ViewerCrown.vue'),
-    functionName: 'Crown',
-    functionCode: 'ViewerCrown',
-    functionId: '102',
-  },
+  // 'IMPLANT': {
+  //   component: () => import('../views/ViewerImplant.vue'),
+  //   functionName: 'Implant',
+  //   functionCode: 'ViewerCT',
+  //   functionId: '101',
+  // },
+  // 'CROWN': {
+  //   component: () => import('../views/ViewerCrown.vue'),
+  //   functionName: 'Crown',
+  //   functionCode: 'ViewerCrown',
+  //   functionId: '102',
+  // },
   'NIGHT': {
     component: () => import('../views/one/NightGuard.vue'),
     functionName: 'NightGuard',
@@ -47,12 +47,12 @@ export const mapComponent2LocalFile = {
     functionCode: 'OcclusionRecovery',
     functionId: '108',
   },
-  'BRACKER': {
-    component: () => import('../views/one/BrackerRemove.vue'),
-    functionName: 'BracketRemoval',
-    functionCode: 'ViewerBracket',
-    functionId: '104',
-  },
+  // 'BRACKER': {
+  //   component: () => import('../views/one/BrackerRemove.vue'),
+  //   functionName: 'BracketRemoval',
+  //   functionCode: 'ViewerBracket',
+  //   functionId: '104',
+  // },
   'RETAINER': {
     component: () => import('../views/ViewerRetainer.vue'),
     functionName: 'Retainer',
